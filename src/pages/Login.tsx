c96e5aa685b92becae24f7149cd5fac41aa83620
dune <nathydune@gmail.com>
2025-04-23 21:07:46 +0200

    suppression video

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,18 +10,6 @@ const Login: React.FC = () => {
   return (
     <div className={Styles.container}>
       <div className={Styles.contant}>
-        <div className={Styles.player}>
-          <video
-            autoPlay
-            muted
-            loop
-            playsInline
-            width="100%"
-            style={{ maxHeight: '95vh' }}
-          >
-            <source src="/video/video.mp4" type="video/mp4" />
-          </video>
-        </div>
         <div className={Styles.login}>
           <LoginConnect />
         </div>
